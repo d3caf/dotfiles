7f05a58d8358225b5b5c723b5a7e4ca381182b69
Andrew Anguiano <aanguiano@newrelic.com>
2024-02-27 11:04:20 -0500

    nvim and kitty tweaks

--- dot_config/nvim/lua/plugins/customize.lua
+++ dot_config/nvim/lua/plugins/customize.lua
@@ -8,4 +8,46 @@ return {
       },
     },
   },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      dim_inactive = true,
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.WinSeparator = { fg = c.blue }
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    },
+  },
 }
